f60d554ca5fd983d2f7e88ecb2e7c5aa06e645be
Sarah Kim <ksarahh95@gmail.com>
2017-02-15 14:42:51 -0800

    week-6-lab-assignment

---
+++ exercise-6/name_fav.py
@@ -0,0 +1,9 @@
+#import system
+import sys
+
+#define variables
+name = sys.argv[1]
+fav_fruit = sys.argv[2]
+
+#print a sentence using the variable
+print("Hi. My name is " + name + ", I like " + fav_fruit)
