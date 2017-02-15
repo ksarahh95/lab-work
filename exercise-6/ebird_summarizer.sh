7ce97b27b5df37428267cd4056b23f7fadfc9707
Sarah Kim <ksarahh95@gmail.com>
2017-02-15 15:26:05 -0800

    week-6-lab-assignment

--- exercise-6/ebird_summarizer.sh
+++ exercise-6/ebird_summarizer.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 # the next line will create a new file called formatted_eBird_data.csv
-replace_newlines.sh eBird_data.csv
+in_class_replace_newlines.sh eBird.csv
 # the next line will replace all extra commas and will replace the contents of formatted_eBird_data.csv
 sed 's/,\s/ /g' formatted_eBird_data.csv > formatted_eBird_data.csv
+python does_something.py reformatted_eBird_data.csv
